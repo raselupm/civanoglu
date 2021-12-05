48711f8982bea07547d82880e777510b4dc7dfbf
Rasel Ahmed <wp@perfectpointmarketing.com>
2021-12-05 12:56:45 +0300

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,19 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import jQuery from './jquery-3.6.0.slim.min';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+jQuery(window).scroll(function() {
+    const scroll = jQuery(window).scrollTop();
+
+    if (scroll >= 50) {
+        jQuery('.sticky-header').addClass('sticky-header-active');
+    } else {
+        jQuery('.sticky-header').removeClass('sticky-header-active');
+    }
+});
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed w-full py-4 px-12 flex justify-between items-center z-30">
+<div class="fixed w-full py-4 px-12 flex justify-between items-center z-30 sticky-header">
     <div class="min-w-max">
         <img width="100" src="/img/logo.jpeg" alt="">
     </div>
