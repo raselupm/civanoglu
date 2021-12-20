dae7778c34ba6c926336e95fea1149f928060fc9
Rasel Ahmed <raselahmed7@gmail.com>
2021-12-20 19:18:53 +0300

    Update

--- resources/views/components/single-property-card.blade.php
+++ resources/views/components/single-property-card.blade.php
@@ -6,7 +6,7 @@
         <div class="py-20 bg-center" style="background-image: url('/img/hero-bg.jpg')"></div>
         <div class="p-3">
             <h2 class="leading-0 text-base">{{$property->name}}</h2>
-            <h3 class="text-2xl py-3">{{$_GET['currency'] == 'usd' ? intval($property->lira_to_usd($property->price)) . ' USD' : $property->price . ' TL'}}</h3>
+            <h3 class="text-2xl py-3">{{isset($_GET['currency']) && $_GET['currency'] == 'usd' ? intval($property->lira_to_usd($property->price)) . ' USD' : $property->price . ' TL'}}</h3>
             <div class="border-t-2">
                 <ul class="flex items-center -mx-1 my-4">
                     <li class="px-2 py-1 bg-blue-50 rounded-md text-xs mx-1 shadow-sm">{{$property->bedrooms}} bedrooms</li>
