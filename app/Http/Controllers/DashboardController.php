38788cecb75522f4415edc2bb07d1ad415a9f82d
Rasel Ahmed <wp@perfectpointmarketing.com>
2021-12-25 20:42:28 +0300

    property update fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,8 +29,6 @@ class DashboardController extends Controller
         return [
             'name' => 'required',
             'name_tr' => 'required',
-            'featured_image' => 'required|image',
-            'gallery_images' => 'required',
             'location_id' => 'required',
             'price' => 'required|integer',
             'sale' => 'integer',
@@ -45,13 +43,7 @@ class DashboardController extends Controller
         ];
     }
 
-    public function createProperty(Request $request) {
-
-        $request->validate($this->validateProperty());
-
-
-
-        $property = new Property();
+    public function saveOrUpdateProperty($property, $request) {
         $property->name = $request->name;
         $property->name_tr = $request->name_tr;
 
@@ -61,8 +53,6 @@ class DashboardController extends Controller
         $request->featured_image->storeAs('public/uploads', $featured_image_name);
 
         $property->featured_image = $featured_image_name;
-
-
 
 
         $property->location_id = $request->location_id;
@@ -94,6 +84,21 @@ class DashboardController extends Controller
             $media->property_id = $property->id;
             $media->save();
         }
+    }
+
+    public function createProperty(Request $request) {
+        $updated_validation = $this->validateProperty()[] = [
+            'featured_image' => 'required|image',
+            'gallery_images' => 'required',
+        ];
+
+        $request->validate($updated_validation);
+
+
+
+        $property = new Property();
+
+        $this->saveOrUpdateProperty($property, $request);
 
         return redirect(route('dashboard-properties'))->with(['message' => 'Property is added.']);
     }
@@ -124,46 +129,7 @@ class DashboardController extends Controller
         $request->validate($this->validateProperty());
 
 
-        $property->name = $request->name;
-        $property->name_tr = $request->name_tr;
-
-
-        $featured_image_name = time() . '-' . $request->featured_image->getClientOriginalName();
-        // store the file
-        $request->featured_image->storeAs('public/uploads', $featured_image_name);
-
-        $property->featured_image = $featured_image_name;
-
-
-        $property->location_id = $request->location_id;
-        $property->price = $request->price;
-        $property->sale = $request->sale;
-        $property->type = $request->type;
-        $property->bedrooms = $request->bedrooms;
-        $property->bathrooms = $request->bathrooms;
-        $property->drawing_rooms = $request->drawing_rooms;
-        $property->net_sqm = $request->net_sqm;
-        $property->gross_sqm = $request->gross_sqm;
-        $property->pool = $request->pool;
-        $property->overview = $request->overview;
-        $property->overview_tr = $request->overview_tr;
-        $property->why_buy = $request->why_buy;
-        $property->why_buy_tr = $request->why_buy_tr;
-        $property->description = $request->description;
-        $property->description_tr = $request->description_tr;
-
-        $property->save();
-
-
-        foreach($request->gallery_images as $image) {
-            $gallery_image_name = time() . '-' . $image->getClientOriginalName();
-            $image->storeAs('public/uploads', $gallery_image_name);
-            $media = new Media();
-            $media->name = $gallery_image_name;
-
-            $media->property_id = $property->id;
-            $media->save();
-        }
+        $this->saveOrUpdateProperty($property, $request);
 
         return redirect(route('dashboard-properties'))->with(['message' => 'Property is saved.']);
     }
