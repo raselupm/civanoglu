bf23a2a5596e00f4ff589724f01950eed5451909
Rasel Ahmed <wp@perfectpointmarketing.com>
2022-01-01 12:31:54 +0300

    github action

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="relative z-10 pt-48 pb-52 bg-cover bg-center" style="background-image: url(/img/hero-bg.jpg)">
         <div class="absolute h-full w-full bg-black opacity-70 top-0 left-0 z-10"></div>
         <div class="container relative z-20 text-white text-center text-2xl">
-            <h2 class="font-bold text-5xl mb-8">Find your best property,<br/> And live quality life.</h2>
+            <h2 class="font-bold text-5xl mb-8">Find your best property,<br/> And live quality life. - Test</h2>
             <p>We will try finding the best one for you. Please search using the form below.</p>
         </div>
     </div>
