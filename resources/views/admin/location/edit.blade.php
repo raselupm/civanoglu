38c93cabc1dd00554f7d7f1f37677053e8815713
Rasel Ahmed <wp@perfectpointmarketing.com>
2022-01-02 09:11:11 +0300

    turkish translate

--- resources/views/admin/location/edit.blade.php
+++ resources/views/admin/location/edit.blade.php
@@ -2,11 +2,11 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Edit location') }}
+                {{ __('Konumunu düzenle') }}
             </h2>
 
             <div class="min-w-max">
-                <a href="{{route('dashboard-location.index')}}" class="fullwidth-btn">Back</a>
+                <a href="{{route('dashboard-location.index')}}" class="fullwidth-btn">Geri</a>
             </div>
         </div>
     </x-slot>
@@ -16,7 +16,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <form action="{{route('dashboard-location.update', $location->id)}}" method="post" class="p-6 bg-white border-b border-gray-200" enctype="multipart/form-data"> @csrf @method('put')
                     <div class="mb-6">
-                        <label class="civanoglu-label" for="name">Title <span class="required-text">*</span></label>
+                        <label class="civanoglu-label" for="name">Başlık <span class="required-text">*</span></label>
                         <input class="civanoglu-input" type="text" id="name" name="name" value="{{$location->name}}" required>
 
                         @error('name')
@@ -26,7 +26,7 @@
 
 
 
-                    <button class="btn" type="submit">Save</button>
+                    <button class="btn" type="submit">Kayıt etmek</button>
                 </form>
             </div>
         </div>
--- resources/views/admin/location/index.blade.php
+++ resources/views/admin/location/index.blade.php
@@ -2,11 +2,11 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Locations') }}
+                Konumlar
             </h2>
 
             <div class="min-w-max">
-                <a href="{{route('dashboard-location.create')}}" class="fullwidth-btn">Add New Location</a>
+                <a href="{{route('dashboard-location.create')}}" class="fullwidth-btn">Yeni Konum Ekle</a>
             </div>
         </div>
 
@@ -19,8 +19,8 @@
                     <table class="w-full table-auto mb-6">
                         <thead>
                         <tr>
-                            <th class="border px-4 py-2">Name</th>
-                            <th style="width: 250px" class="border px-4 py-2">Actions</th>
+                            <th class="border px-4 py-2">İsim</th>
+                            <th style="width: 300px" class="border px-4 py-2">Hareketler</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -28,10 +28,10 @@
                             <tr>
                                 <td class="border px-4 py-2">{{$location->name}}</td>
                                 <td class="border px-4 py-2 text-center">
-                                    <a class="bg-blue-500 text-white px-4 py-2 text-xs rounded" href="{{route('dashboard-location.edit', $location->id)}}">Edit</a>
-                                    <a class="bg-green-500 text-white px-4 py-2 text-xs rounded" href="{{route('single-location', $location->id)}}" target="_blank">View</a>
-                                    <form onsubmit="return confirm('Do you really want to delete the location?');" action="{{route('dashboard-location.destroy', $location->id)}}" method="post" class="inline-block"> @csrf @method('delete')
-                                        <button style="height: 27px;top:1.5px" type="submit" class="bg-red-500 text-white px-4 py-2 text-xs rounded relative">Delete</button>
+                                    <a class="bg-blue-500 text-white px-4 py-2 text-xs rounded" href="{{route('dashboard-location.edit', $location->id)}}">Düzenlemek</a>
+                                    <a class="bg-green-500 text-white px-4 py-2 text-xs rounded" href="{{route('single-location', $location->id)}}" target="_blank">görüş</a>
+                                    <form onsubmit="return confirm('Konumu gerçekten silmek istiyor musunuz?');" action="{{route('dashboard-location.destroy', $location->id)}}" method="post" class="inline-block"> @csrf @method('delete')
+                                        <button style="height: 27px;top:1.5px" type="submit" class="bg-red-500 text-white px-4 py-2 text-xs rounded relative">Silmek</button>
                                     </form>
                                 </td>
                             </tr>
